87be58bb16b26a9d3fdf6eef407cc068bc762d61
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2023-09-14 09:59:34 +0200

    Add the DAG and make it simple and functional to send an email
    Adding a dag with task and dag structure. By using the function
    from the sendEmail program it can send an email. This DAG can
    now be run via docker if you add your email as recipient_email.
    
    Use docker compose up to start airflow. Go to localhost:8080
    
    Search for your dag in the search bar. It is called Email_me_info
    Once you run the program it should send the recipient and email
    with a random number from 1-10 and some other text.

---
+++ dags/scrapeAndEmailDag.py
@@ -0,0 +1,39 @@
+from airflow.operators.python import task
+from airflow.models import DAG
+import datetime as dt
+from random import randint
+import sendEmail
+
+# Write the recipient of the email here, I suggest you test it with your own email to try it out.
+@task
+def retrieveRecipient():
+    return ''
+
+# This function actually writes the email. The sender is hardcoded but can be changed in 
+# sendEmail.py if you wish to send the email from another email adress.
+@task
+def send_email_task(recipient_email, subject, message):
+    sendEmail.send_email(recipient_email, subject, message)
+
+# I will add the scraping function here later on.
+@task
+def retrieveMessage():
+    return f"This is your random number between 1 and 10: {randint(1,10)}"
+
+@task
+def retrieveSubject():
+    return "This is an email from an Airflow DAG"
+
+
+with DAG(
+   "Email_Me_Info",
+   default_args={'owner': 'airflow'},
+   start_date=dt.datetime(2023,8,9),
+   schedule=None,
+   catchup=False
+) as dag:
+   message = retrieveMessage()
+   subject = retrieveSubject()
+   recipient_email = retrieveRecipient()
+   
+   send_email_task(recipient_email,subject,message)
