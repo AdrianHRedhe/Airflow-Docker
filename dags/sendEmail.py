2bd0bf048339dac1325db5db76c014260d753664
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2023-09-14 09:48:54 +0200

    Add the sendEmail.py program will work as import for the DAG
    Added a sendEmail python program to be imported to the DAG
    To keep the code in the DAG uncluttered.

---
+++ dags/sendEmail.py
@@ -0,0 +1,42 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# This function sends and email to the the recipient email
+# The sender email is hardcoded, but easily interchangable
+# if you wish to make a function where the email can be 
+# changed.
+
+def send_email(recipient_email, subject, message):
+    # Provide your Gmail credentials and the email details
+    sender_email = '1234.airflow.example@gmail.com'
+    sender_password = 'qtgexzgqsbggydxm'
+    
+    # Here you can replace the email with your own if you want the program to send an email
+    recipient_email = str(recipient_email)
+    subject = str(subject)
+    message = str(message)
+
+    # Set up the SMTP server
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+
+    # Create a secure connection to the SMTP server
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+
+    # Login to your Gmail account
+    server.login(sender_email, sender_password)
+
+    # Create the email message
+    email_message = MIMEMultipart()
+    email_message['From'] = sender_email
+    email_message['To'] = recipient_email
+    email_message['Subject'] = subject
+    email_message.attach(MIMEText(message, 'plain'))
+
+    # Send the email
+    server.send_message(email_message)
+
+    # Disconnect from the server
+    server.quit()
