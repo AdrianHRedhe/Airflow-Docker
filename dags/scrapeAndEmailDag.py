d625d8404cfd38db33a53875ff922401190df811
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2023-09-14 13:05:37 +0200

    Add the use of attachment in the DAG. Preparation for testing scraping
    Add a simple CSV file to be attached when the airflow DAG runs.
    This will be replaced with the CSV file generated by the scrape.

--- dags/scrapeAndEmailDag.py
+++ dags/scrapeAndEmailDag.py
@@ -1,6 +1,7 @@
 from airflow.operators.python import task
 from airflow.models import DAG
 import datetime as dt
+import pandas as pd
 from random import randint
 import sendEmail
 
@@ -12,8 +13,8 @@ def retrieveRecipient():
 # This function actually writes the email. The sender is hardcoded but can be changed in 
 # sendEmail.py if you wish to send the email from another email adress.
 @task
-def send_email_task(recipient_email, subject, message):
-    sendEmail.send_email(recipient_email, subject, message)
+def send_email_task(recipient_email, subject, message,attachment):
+    sendEmail.send_email(recipient_email, subject, message,attachment)
 
 # I will add the scraping function here later on.
 @task
@@ -23,6 +24,10 @@ def retrieveMessage():
 @task
 def retrieveSubject():
     return "This is an email from an Airflow DAG"
+
+@task
+def retrieveAttachment():
+    return pd.DataFrame({'TestData':[1,2,3,4,5]})
 
 
 with DAG(
@@ -35,5 +40,6 @@ with DAG(
    message = retrieveMessage()
    subject = retrieveSubject()
    recipient_email = retrieveRecipient()
+   attachment = retrieveAttachment()
    
-   send_email_task(recipient_email,subject,message)
+   send_email_task(recipient_email,subject,message,attachment)
