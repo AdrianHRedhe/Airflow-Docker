755ae71ad8c8c57c105d7a416e8277cdaee0d197
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2023-09-18 11:56:30 +0200

    Add function scrapeTables() to scrapeCSV.py
    This function is a reworked version from the selenium.ipynb notebook.
    It takes a webdriver as an input. It is then hardcoded to find the
    wheather information on the specific website SMHI. And outputs this
    information.

--- dags/scrapeCSV.py
+++ dags/scrapeCSV.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # This function instansiates the remote driver. 
 # Using the container name and the port 4444 both defined in
@@ -24,3 +25,30 @@ def instantiateRemoteDriver():
     driver = webdriver.Remote(f'{remote_webdriver}:4444/wd/hub',options=options)
 
     return driver
+
+# This is hardcoded to return the weather data from the site SMHI.
+# In another example it might be better to leave things open ended
+# But in this case I have a very explicit use case and will not
+# showcase that.
+
+def scrapeTables(driver):
+
+    # Go to the website
+    driver.get('https://www.smhi.se/vader/prognoser/ortsprognoser/q/Stockholm/2673730')
+
+    # Find the pane where the individual days are located.
+    root = driver.find_element(By.ID,'root')
+    pane = root.find_elements(By.XPATH,'//*[@role="tabpanel"]')[0]
+
+    # You need to click the buttons to be able to retrieve the weather data
+    buttons = pane.find_elements(By.XPATH,'./Button')
+
+    # Lets get the weather data for the rest of today and tomorrow by first clicking these days.
+    buttons[0].click()
+    buttons[1].click()
+
+    # On the pane the weather data is now visible
+    # and we can scrape the information.
+    tables = pane.find_elements(By.XPATH,'./div/div/table/tbody')
+
+    return tables
