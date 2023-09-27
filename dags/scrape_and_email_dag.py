0ea0268c07d6103728b755cb83cbe89966353863
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2023-09-27 10:21:33 +0200

    Remove function retrieve_subject(), add subject in send_email_task
    This function was only returning a hardcoded subjectline for the
    email. It felt a bit superfluous as an airflow task and was therefore
    removed. Instead the subject was added in the send_email_task task

--- dags/scrape_and_email_dag.py
+++ dags/scrape_and_email_dag.py
@@ -14,8 +14,9 @@ def retrieve_recipient():
 # This function actually writes the email. The sender is hardcoded but can be changed in 
 # sendEmail.py if you wish to send the email from another email adress.
 @task
-def send_email_task(recipient_email, subject, message,attachment):
-    send_email(recipient_email, subject, message,attachment)
+def send_email_task(recipient_email, message, attachment):
+    subject = "This is an email from an Airflow DAG"
+    send_email(recipient_email, subject, message, attachment)
 
 # This function could be used to send an alternative message, as an example,
 # it could be used to highlight some information from the Scraped CSV
@@ -24,10 +25,6 @@ def retrieve_message(attachment):
     weather_tomorrow = attachment[attachment['daysFromNow'] == 1]
     max_temperature = weather_tomorrow.degree.max()
     return f"Here comes your scraped CSV file, tomorrow the highest temperature will be {max_temperature}. \n"
-
-@task
-def retrieve_subject():
-    return "This is an email from an Airflow DAG"
 
 # This function runs the scrape that is described in the helper function scrapeCSV.py
 # and returns a CSV file with the scraped information.
@@ -44,9 +41,8 @@ with DAG(
    schedule=None, # use '0 6 * * *' to run the program at 6 am UTC everyday
    catchup=False
 ) as dag:
-   subject = retrieve_subject()
    recipient_email = retrieve_recipient()
    attachment = retrieve_attachment()
    message = retrieve_message(attachment)
    
-   send_email_task(recipient_email,subject,message,attachment)
+   send_email_task(recipient_email, message, attachment)
